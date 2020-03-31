cd68f09528db71b85787f3064949270ddecba41d
abirami-mallikarjuna <62918650+abirami-mallikarjuna@users.noreply.github.com>
2020-03-31 21:26:03 +0530

    update run.sh

--- install/run.sh
+++ install/run.sh
@@ -3,4 +3,4 @@ do
 ( rpm -q $i ) || yum install $i -y 
 done
 ansible-pull -i localhost -U https://github.com/abirami-mallikarjuna/devops.git install/docker.yml 
-
+ansible-pull -i localhost -U https://github.com/abirami-mallikarjuna/devops.git jenkins/jenkins.yml

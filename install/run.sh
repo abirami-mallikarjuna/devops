de35713aad5c53c87147312b061d1862e331f691
Your Name <you@example.com>
2020-03-31 14:33:47 +0000

    changed path

--- install/run.sh
+++ install/run.sh
@@ -2,5 +2,5 @@ for i in git ansible
 do
 ( rpm -q $i ) || yum install $i -y 
 done
-ansible-pull -i localhost -U https://github.com/abirami-mallikarjuna/devops.git docker.yml 
+ansible-pull -i localhost -U https://github.com/abirami-mallikarjuna/devops.git install/docker.yml 
 

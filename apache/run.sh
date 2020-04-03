09ab9b3952a570645f8e3b3986da515991354fbe
Your Name <you@example.com>
2020-04-03 16:39:34 +0000

    upload

--- apache/run.sh
+++ apache/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+yum install git -y
+
+git clone https://github.com/abirami-mallikarjuna/devops.git
+
+
+cd devops/apache
+
 docker-compose build --no-cache
 sleep 60
 docker-compose up -d 

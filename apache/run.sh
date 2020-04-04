3ff97c8b53bdf2a1cffaee04fd84be5ec613aea3
Your Name <you@example.com>
2020-04-04 13:34:06 +0000

    change1

--- apache/run.sh
+++ apache/run.sh
@@ -6,11 +6,11 @@
 git clone https://github.com/abirami-mallikarjuna/devops.git
 
 ansible-pull -i localhost -U https://github.com/abirami-mallikarjuna/devops.git apache/docker.yml
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 cd devops/apache
 
 docker-compose build --no-cache
-sleep 60
+sleep 20
 docker-compose up -d 
